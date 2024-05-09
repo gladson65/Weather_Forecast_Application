21e81e737f0e486d402d57e73826364a3f18daf8
gladson65 <grauth65@gmail.com>
2024-05-09 15:49:48 +0530

    Current Location search feature added

--- main.js
+++ main.js
@@ -74,8 +74,6 @@ window.onload = function() {
         
         // disabled input field
         document.querySelector(".inputValue").disabled = true;
-        // data preparation to store into the local storage
-        
         
         
         
@@ -308,7 +306,7 @@ window.onload = function() {
             
         }    
         else {
-            console.log("Please provide a city name")
+            console.log("Please provide a city name");
         }
 
         
@@ -330,10 +328,256 @@ window.onload = function() {
     })
 
 
+    // get user current location
+    let currentLat;
+    let currentLon;
+    let currentButton = document.getElementById("current-location");
+    currentButton.addEventListener("click", function (e) {
+        e.preventDefault();
 
+        // change text of the current location button
+        let currentButton = document.getElementById("current-location");
+        currentButton.innerHTML = "Refresh";
+
+        // Refresh
+        if (currentButton.innerHTML == "Refresh") {
+            let currentButton = document.getElementById("current-location");
+            currentButton.classList.add("current-refresh");
+
+            const currentRefresh = document.querySelector(".current-refresh");
+            currentRefresh.addEventListener("click", () =>{
+                location.reload();
+            })
+        }
+
+        // getting latitude and longitude
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                let currentLat = position.coords.latitude;
+                let currentLon = position.coords.longitude;
+                
+                // fetching data by longitude and latitude
+                function fetchData(callback) {
+
+                    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${currentLat}&lon=${currentLon}&units=metric&appid=bfc3d2331cbfea5b3fffe45863963901`)
+                        .then(response => response.json())
+                        .then(data => {
+
+                            
+
+                            // filter the forcasts for five days
+                            const forcastDays = [];
+                            const fiveForcasts = data.list.filter((forecast) => {
+                                const forecastDate = new Date(forecast.dt_txt).getDate();
+                                if (!forcastDays.includes(forecastDate)) {
+                                    return forcastDays.push(forecastDate);
+                                }
+                            })
+
+                            console.log(forcastDays);
+
+                            const todayForcast = [];
+                            const today = data.list.filter((todayReport) => {
+                                const weatherDate = new Date(todayReport.dt_txt).getDate();
+                                const todayDate = new Date().getDate();
+                                // const liveTime = new Date().getHours();
+                                if (weatherDate == todayDate) {
+                                    return todayForcast.push(todayReport);
+                                }
+                            })
+
+                            const currentArr = [];
+                            const currentTime = todayForcast.filter((current) => {
+                                const weatherTime = new Date(current.dt_txt).getHours();
+                                const liveTime = new Date().getHours();
+                                if (weatherTime < liveTime) {
+                                    return currentArr.push(current)
+                                }
+                            })
+
+
+                            const liveWeather = [currentArr[currentArr.length - 1]];
+
+                            // show live data
+                            // weather icon
+                            const img = document.getElementById("w-icon");
+                            img.src = `https://openweathermap.org/img/wn/${liveWeather[0].weather[0].icon}@2x.png`;
+
+                            // Day or Night
+                            // time
+                            let partDayP = document.getElementById("partDay");
+                            let hours = new Date().getHours();
+                            partDayP.innerHTML = `${hours}`;
+
+                            if (hours > 12 && hours < 18) {
+                                partDayP.innerHTML = 'Afternoon';
+                            }
+                            else if (hours >= 18 && hours < 20) {
+                                partDayP.innerHTML = 'Evening';
+
+                            }
+                            else if (hours >= 20 && hours < 5) {
+                                partDayP.innerHTML = 'Night';
+
+                            }
+                            else if (hours >= 5 && hours <= 12) {
+                                partDayP.innerHTML = 'Morning';
+
+                            }
+
+                            // day
+                            const day = new Date().getDay();
+                            const dayArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                            const toDay = dayArr[day];
+
+                            const dayP = document.getElementById("toDay");
+                            dayP.innerHTML = `${toDay}`
+
+
+                            // description
+                            const describP = document.getElementById("description");
+                            const description = liveWeather[0].weather[0].description;
+                            describP.innerHTML = `${description}`;
+
+                            // visibility
+                            const visibleP = document.createElement("p");
+                            const visibility = Math.trunc(liveWeather[0].visibility / 1000);
+                            visibleP.innerHTML = `visibility: ${visibility} Km`;
+                            describP.appendChild(visibleP);
+
+
+                            // temperature
+                            const temp = document.getElementById("temp");
+                            temperature = liveWeather[0].main.temp;
+                            temp.innerHTML = `  ${Math.trunc(temperature)}&deg`
+                            temp.style.fontSize = "30px";
+
+                            // wind Speed
+                            const windP = document.getElementById("wind");
+                            const wind = liveWeather[0].wind.speed;
+                            windP.innerHTML = `  ${Math.round(wind)} m/s`;
+                            windP.style.fontSize = "30px";
+
+                            // humidity
+                            const humidP = document.getElementById("humidity");
+                            const humidity = liveWeather[0].main.humidity;
+                            humidP.innerHTML = `  ${Math.round(humidity)} %`;
+                            humidP.style.fontSize = "30px";
+
+
+                            // five day forecasts array passing to the callback function
+                            fiveForcasts.shift();
+                            const newFiveForcasts = [fiveForcasts];
+
+                            //passing data to fiveForcasts function
+                            callback(newFiveForcasts);
+
+
+
+
+                            console.log(liveWeather)
+
+
+                    }).catch((error) => {
+                        const forecastDiv = document.getElementById("five-forecast");
+                        forecastDiv.innerHTML = "<h1 style='text-align: center;'>SORRY, NO DATA FOUND!!! Try leter...</h1>";
+                        forecastDiv.style.color = "red";
+                        forecastDiv.style.textAlign = "center";
+                        console.log(error)
+                    })
+                    
+
+                }
+
+
+
+
+                // loading animation
+                const forecastDiv = document.getElementById("five-forecast");
+                const lDiv = document.createElement("span");
+                lDiv.style.display = "block";
+                lDiv.classList.add("load")
+                forecastDiv.appendChild(lDiv);
+
+
+
+
+                // display five day forecast
+                function fiveForcasts(newFiveForcasts) {
+
+                    const arr = [newFiveForcasts[0]];
+                    lDiv.style.display = "none";
+
+
+                    arr[0].forEach(x => {
+
+                        const forecastDiv = document.getElementById("five-forecast");
+
+                        // date
+                        const dateP = document.createElement("p");
+                        const getDate = x.dt_txt;
+                        const g = getDate.split(" ")
+                        dateP.innerHTML = `${g[0]}`;
+                        dateP.style.textAlign = "center";
+
+                        // weather icon
+                        const icon = document.createElement("img");
+                        icon.src = `https://openweathermap.org/img/wn/${x.weather[0].icon}@2x.png`;
+                        icon.style.margin = "0 auto";
+
+                        // weather description
+                        const wdP = document.createElement("p");
+                        wdP.innerHTML = `${x.weather[0].description}`;
+                        wdP.style.textAlign = "center";
+
+                        const forcastDivs = document.createElement("div");
+                        // temperature
+                        const tempP = document.createElement("p");
+                        tempP.innerHTML = `<i class="fa-solid fa-temperature-high" style="color: yellow; font-size:15px"></i> ${Math.trunc(x.main.temp)}`;
+                        tempP.style.textAlign = "center";
+
+                        // wind
+                        const windP = document.createElement("p");
+                        windP.innerHTML = `<i class="fa-solid fa-wind" style="color: black; font-size:15px"></i> ${Math.round(x.wind.speed)}`;
+                        windP.style.textAlign = "center";
+
+                        //humidity
+                        const humidP = document.createElement("p");
+                        humidP.innerHTML = `<i class="fa-solid fa-droplet" style="color: black; font-size:15px"></i> ${Math.round(x.main.humidity)}`;
+                        humidP.style.textAlign = "center";
+
+                        forcastDivs.appendChild(dateP);
+                        forcastDivs.appendChild(icon);
+                        forcastDivs.appendChild(wdP);
+                        forcastDivs.appendChild(tempP);
+                        forcastDivs.appendChild(windP);
+                        forcastDivs.appendChild(humidP);
+
+
+                        forecastDiv.appendChild(forcastDivs);
+
+                    })
+
+
+
+                }
+
+                fetchData(fiveForcasts);
+                
+            },
+                (error) => {
+                    console.log("error", error)
+                }
+            );
+
+
+        }
+        else {
+            console.log("Geolocation is not supported bt this browser")
+        }
+
+
+    })
 }
 
-// const time = data.list[0].dt_txt;
-// const timeArr = new Date(time).getHours();
-// const liveTime = new Date().getHours();
-// console.log(timeArr < liveTime);
+
